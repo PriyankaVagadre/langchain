9ca2d942b1c5b9ffcf687351f9069a18056a66a5
priyankavagadre <priyankavagadre@hotmail.com>
2026-06-16 22:43:18 +0530

    install the latest version thrugh requirment txt

---
+++ README.md
@@ -0,0 +1,5 @@
+Create project
+run uv init 
+run uv venv
+run .venv/bin/activate ### to activate the env
+source .venv/bin/activate
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from langchain!")
+    print("Hello from langchainupdated!")
 
 
 if __name__ == "__main__":
